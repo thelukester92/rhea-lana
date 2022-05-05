9050b9ae2e5aad68ad10654371d0841a5b56ca91
Luke Godfrey <thelukester92@me.com>
2022-05-05 14:33:40 -0500

    update preview

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -6,15 +6,19 @@ export const upload = async (consignerId, password, batchId, filePath, { dryRun,
     if (dryRun) {
         console.log('===== DRY RUN =====');
     }
+
+    offset = offset || 0;
     if (offset) {
         data = data.slice(offset);
     }
+
     const client = new RheaLanaClient();
     if (!dryRun) {
         await client.signIn(consignerId, password, batchId);
     }
+
     for (const [i, item] of data.entries()) {
-        console.log(`uploading item ${i + 1} of ${data.length}...`);
+        console.log(`uploading item ${i + 1 + offset} of ${data.length + offset}...`);
         console.log(`    ${item.desc}, Size ${item.size}, Price $${item.price}`);
         if (!dryRun) {
             await client.createItem(item);
